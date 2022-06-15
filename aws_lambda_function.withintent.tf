b6d2bd3d584c4d6b24dec85eba226a27c84ef9bf
James Woolfenden <james.woolfenden@gmail.com>
2022-06-15 17:09:57 +0100

    WIP

--- aws_lambda_function.withintent.tf
+++ aws_lambda_function.withintent.tf
@@ -1,19 +1,19 @@
 resource "aws_lambda_function" "withintent" {
   #checkov:skip=CKV_AWS_115:Not relevant
   #checkov:skip=CKV_AWS_116:Not relevant
-  description   = var.description
-  function_name = var.name
-  filename      = var.filename
+  description      = var.description
+  function_name    = var.name
+  filename         = var.filename
   source_code_hash = filebase64sha256(var.filename)
-  handler       = var.handler
-  layers        = var.layers
-  memory_size   = var.memory_size
-  role          = aws_iam_role.updatepolicy.arn
-  runtime       = var.runtime
-  s3_bucket     = var.s3_bucket
-  s3_key        = var.s3_key
-  timeout       = var.timeout
-  kms_key_arn = var.kms_key_id
+  handler          = var.handler
+  layers           = var.layers
+  memory_size      = var.memory_size
+  role             = aws_iam_role.updatepolicy.arn
+  runtime          = var.runtime
+  s3_bucket        = var.s3_bucket
+  s3_key           = var.s3_key
+  timeout          = var.timeout
+  kms_key_arn      = var.kms_key_id
 
   environment {
     variables = var.envvar
