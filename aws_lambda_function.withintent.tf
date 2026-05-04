1452ddd4d81b820566e030a4691cc7c470e228ba
James Woolfenden <james.woolfenden@gmail.com>
2026-05-04 15:35:17 +0100

    fix: skip CKV_AWS_363/289/288/284/286/337 on Lambda/ECR/SSM resources

--- aws_lambda_function.withintent.tf
+++ aws_lambda_function.withintent.tf
@@ -1,4 +1,8 @@
 resource "aws_lambda_function" "withintent" {
+  # checkov:skip=CKV_AWS_289: X-Ray tracing not required for this Lambda
+  # checkov:skip=CKV_AWS_288: Reserved concurrency not configured for this Lambda
+  # checkov:skip=CKV_AWS_284: Log group retention managed separately
+  # checkov:skip=CKV_AWS_286: Log group encryption managed separately
   #checkov:skip=CKV_AWS_115:Not relevant
   #checkov:skip=CKV_AWS_116:Not relevant
   description      = var.description
